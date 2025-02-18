98c84bf0c4c84d5a159364c4671c2e34a58d3446
fartbagxp <fartbagxp@gmail.com>
2025-02-17 23:30:02 -0500

    Dev/doge savings (#1)
    * chore[dev]: saving records
    
    * chore[dev]: more files
    
    * Adding jq to the mix

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+[[ -v VERBOSE ]] && set -x
+set -eu
+
+target="${1}/data/raw"
+
+echo "doge savings - contracts and leases"
+curl --retry 5 --max-time 180 -f https://www.doge.gov/api/receipts/overview \
+  --compressed \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+  -H 'Accept-Language: en-US,en;q=0.5' \
+  -H 'Accept-Encoding: gzip, deflate' | jq '.' > "${target}/doge_savings.json"
+
+jq .contracts "${target}/doge_savings.json" > "${target}/doge_contracts_termination.json"
+jq .leases "${target}/doge_savings.json" > "${target}/doge_leases_termination.json"
