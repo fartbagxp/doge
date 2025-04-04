63718e6233616c376c7763718e0c0c32a4dc7cee
fartbagxp <fartbagxp@gmail.com>
2025-04-04 17:58:59 -0400

    Using the DOGE API to pull dataset (#5)

---
+++ client.py
@@ -0,0 +1,39 @@
+from urllib.parse import urlencode
+from curl_cffi import requests
+
+class DogeClient:
+  def __init__(self, base_url="https://api.doge.gov"):
+    self.base_url = base_url
+    self.session = requests.Session()
+
+  def _get_paginated(self, path, params=None):
+    all_items = []
+    page = 1
+    while True:
+      q = params.copy() if params else {}
+      q["page"] = page
+      q["per_page"] = 500
+      q["sort_by"] = "date"
+      url = f"{self.base_url}{path}?{urlencode(q)}"
+      resp = self.session.get(url)
+      data = resp.json()
+      if not data.get("success"):
+        break
+      results = list(data.get("result", {}).values())[0]
+      all_items.extend(results)
+      if len(results) < 500:
+        break
+      page += 1
+    return all_items
+
+  def get_grant_savings(self, **kwargs):
+    return self._get_paginated("/savings/grants", kwargs)
+
+  def get_contract_savings(self, **kwargs):
+    return self._get_paginated("/savings/contracts", kwargs)
+
+  def get_lease_savings(self, **kwargs):
+    return self._get_paginated("/savings/leases", kwargs)
+
+  def get_payments(self, **kwargs):
+    return self._get_paginated("/payments", kwargs)
